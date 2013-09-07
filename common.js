c107fea63edd0f36786740d19aae61d02be92529
nwieczorek <nwieczorek@gmail.com>
2013-09-07 12:45:31 -0500

    added character tiles

---
+++ common.js
@@ -0,0 +1,52 @@
+
+
+function log(message){
+   try{
+      console.log(message);
+   } catch (exception){
+      return;
+   }
+}
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+function getMousePos(canvas, evt){
+   var rect = canvas.getBoundingClientRect();
+   return { x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top};
+}
+
+/*---------------------------------------------------------------
+ * Point class
+ */
+function Point(x,y){
+   this.x = x;
+   this.y = y;
+}
+Point.prototype.toString = function(){
+   return "(" + this.x + "," + this.y + ")";
+}
+Point.prototype.adjacent = function(other){
+   return (((this.x == other.x) && ((this.y + 1 == other.y) || (this.y - 1 == other.y))) || 
+            ((this.y == other.y) && ((this.x + 1 == other.x) || (this.x - 1 == other.x))));
+}
+
+Point.prototype.add = function(other){
+   return new Point( this.x + other.x, this.y + other.y);
+}
+
+Point.prototype.equal = function(other){
+   return this.x == other.x && this.y == other.y;
+}
+
+Point.UP = new Point(0,-1);
+Point.DOWN = new Point(0,1);
+Point.LEFT = new Point(-1,0);
+Point.RIGHT = new Point(1,0);
